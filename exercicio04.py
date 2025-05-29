6a022e12dd86efe64e280710dcf4b4c60f1d5435
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-05-29 14:53:53 +0000

    exercicio 04 concluido

--- exercicio04.py
+++ exercicio04.py
@@ -2,10 +2,18 @@
 # O programa deve receber a distância percorrida e o tempo gasto, calcular a velocidade e indicar se foi 
 # lenta (<5 km/h), moderada (5 a 10 km/h) ou rápida (>10 km/h).
 
-tempo = int(input("Digite seu tempo gasto."))
-dintancia = int(input("Digite sua distancia percorrida."))
- if = tempo
+tempo = int(input("Digite seu tempo gasto:"))
+distancia = int(input("Digite sua distancia percorrida:"))
 
+velocidadeM = distancia / tempo
+print(f"{velocidadeM:.2f} km/h")
+
+if velocidadeM < 5:
+    print("Velocidade lenta.")
+elif velocidadeM < 10:
+    print("Velocidade modereda.")
+elif velocidadeM > 10:
+    print("velocidade rapida")
 
 
 
