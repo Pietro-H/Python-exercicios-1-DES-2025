7335a57073197a6968e95d2f37a630d031b93f45
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-06-12 12:20:33 +0000

    fiz o exercicio12 e começei o exercicio16.

--- exercicio12.py
+++ exercicio12.py
@@ -1,3 +1,10 @@
 #Peça ao usuário uma senha e verifique se ela contém pelo menos 8 caracteres.
 #Exiba uma mensagem de "Senha válida" ou "Senha muito curta".
 
+senha = input("digite uma senha;")
+
+caracteres = len(senha)
+if caracteres >=8:
+    print("senha válida.")
+elif caracteres <=8:
+    print("senha muito curta.")
--- exercicio16.py
+++ exercicio16.py
@@ -3,3 +3,12 @@
 #Salários até R$ 2000,00: +15%
 #De R$ 2000,01 a R$ 5000,00: +10%
 #Acima de R$ 5000,00: +5%
+
+salario = float(input("Digite Seu salário:"))
+porcentagem = float(input("Digite sua porcentagem:"))
+
+calcule01 = salario /0.15
+calcule02 = salario /0.10
+calcule03 = salario /0.5
+
+if salario
