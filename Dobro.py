2b45ff58d14432b247d4da6f89d8241efe0a55e3
Pietro-H <pietro.henrique.pereira@escola.pr.gov.br>
2025-07-01 12:17:55 +0000

    fiz dois exercicios o drobro e boas vindas

---
+++ Dobro.py
@@ -0,0 +1,8 @@
+def soma(n1,n2):
+    return n1 + n2
+
+v1 = int(input("Digite seu primeiro valor: "))
+v2 = int(input("Digite seu segundo valor: "))
+print("A soma dos valores é: ", soma(v1,v2))
+
+#Feito
---
+++ boas_vindas.py
@@ -0,0 +1,8 @@
+
+def bem_vindo():
+    print("Seja bem vindo ao sistema Pietro Lindão.")
+
+bem_vindo()
+
+#Feito.
+
